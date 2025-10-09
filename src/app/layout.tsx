1fb0119a7db74ed2605e20022c65acfa48b7f0f7
Alejandro Guerrero <alegue17@gmail.com>
2025-10-09 09:12:05 -0600

    background working

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,8 @@ const myFont = localFont({
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className={myFont.className}>
-      <body className="min-h-dvh bg-white text-black">
+      <body className="min-h-dvh text-white">
+        <img src={"/blueprint.jpg"} className="fixed w-full h-dvh object-cover opacity-90"></img>
         <Header />
         <main className="relative pt-16">{children}</main>
       </body>
