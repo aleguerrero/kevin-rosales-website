edbcdf951b1201fe0fbd85450a033d7d313b4058
Alejandro Guerrero <alegue17@gmail.com>
2026-04-20 10:25:22 -0600

    icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,13 @@ import Header from "./Components/Header";
 import "./globals.css";
 import localFont from 'next/font/local'
 
+export const metadata = {
+  icons: {
+    icon: '/kevinicon.svg', // Reference from the /public folder
+    apple: '/kevinicon.svg',
+  },
+};
+
 const mainFont = localFont({
   src: '/../../public/OverusedGrotesk-VF.woff2'
 })
